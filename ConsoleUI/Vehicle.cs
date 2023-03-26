15d088ff79643f3c50520582120088c982725493
Lisa Walters <lisacampiwalters@gmail.com>
2023-03-26 00:29:40 -0600

    wrote vehicle method, and added Car and Motorcycle methods

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle_
+    {
+        public string Year { get; set; } = "2013";
+        public string Make { get; set; } = "default make";
+        public string Model { get; set; } = "default model";
+
+        public abstract void DriveAbstract(); //stubbed out method
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine("This sure is a virtual vehicle!");
+        }
+    }
+}
